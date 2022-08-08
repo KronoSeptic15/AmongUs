8cd20f33bc26cee871a45645d37f9e2f79bb4e4f
KronoSeptic <96399896+KronoSeptic15@users.noreply.github.com>
2022-08-08 15:32:00 -0400

    Add files via upload

---
+++ Patches/Patch.cs
@@ -0,0 +1,19 @@
+﻿using HarmonyLib;
+
+namespace AmongUs.Patches
+{
+    [HarmonyPatch(typeof(GorillaTagManager))]
+    [HarmonyPatch("MyMatIndex", MethodType.Normal)]
+    internal class Patch
+    {
+        static bool Prefix(ref int __result)
+        {
+            if (Plugin.inRoom)
+            {
+                __result = 0;
+                return false;
+            }
+            return true;
+        }
+    }
+}
